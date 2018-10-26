9f3fbed8105afdcdc4200b8793e44239e86ca562
Alex <C_hua9495@hotmail.com>
2018-10-27 02:49:15 +0800

    login done

--- app/src/main/java/com/example/alex/secondhandcarseller/AddCarActivity.java
+++ app/src/main/java/com/example/alex/secondhandcarseller/AddCarActivity.java
@@ -11,6 +11,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -38,29 +40,46 @@ import javax.net.ssl.HttpsURLConnection;
 public class AddCarActivity extends AppCompatActivity {
 
 
-    private EditText editTextCarName,editTextCarPrice, editTextCarYear, editTextMileage;
-    private Spinner spinnerCarBrand,spinnerColor,spinnerCarType;
+    private EditText editTextCarName, editTextCarPrice, editTextCarYear, editTextMileage;
+    private Spinner spinnerCarBrand, spinnerColor, spinnerCarType;
     private Button buttonNext;
+    ArrayAdapter<CharSequence> colorAdap, typeAdap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_car);
 
-        editTextCarName=findViewById(R.id.editTextCarName);
-        editTextCarPrice=findViewById(R.id.editTextCarPrice);
-        editTextCarYear=findViewById(R.id.editTextCarYear);
-        editTextMileage=findViewById(R.id.editTextMileage);
-        spinnerCarBrand=findViewById(R.id.spinnerCarBrand);
-        spinnerColor=findViewById(R.id.spinnerColor);
-        spinnerCarType=findViewById(R.id.spinnerCarType);
-        buttonNext=findViewById(R.id.buttonNext);
+        editTextCarName = findViewById(R.id.editTextCarName);
+        editTextCarPrice = findViewById(R.id.editTextCarPrice);
+        editTextCarYear = findViewById(R.id.editTextCarYear);
+        editTextMileage = findViewById(R.id.editTextMileage);
+        spinnerCarBrand = findViewById(R.id.spinnerCarBrand);
+        spinnerColor = findViewById(R.id.spinnerColor);
+        spinnerCarType = findViewById(R.id.spinnerCarType);
+        buttonNext = findViewById(R.id.buttonNext);
 
-        
+        colorAdap = ArrayAdapter.createFromResource(this, R.array.color, android.R.layout.simple_spinner_item);
+        colorAdap.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerColor.setAdapter(colorAdap);
+
+        typeAdap = ArrayAdapter.createFromResource(this, R.array.car_type, android.R.layout.simple_spinner_item);
+        typeAdap.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCarType.setAdapter(typeAdap);
 
 
 
+        spinnerColor.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), parent.getItemAtPosition(position) + "selected", Toast.LENGTH_LONG).show();
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
     }
 
