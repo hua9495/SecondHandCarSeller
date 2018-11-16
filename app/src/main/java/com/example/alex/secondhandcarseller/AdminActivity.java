b974205f79d79131b9cfbb0526ea5a5d73092ee3
hua9495 <32921471+hua9495@users.noreply.github.com>
2018-11-16 15:56:32 +0800

    admin

--- app/src/main/java/com/example/alex/secondhandcarseller/AdminActivity.java
+++ app/src/main/java/com/example/alex/secondhandcarseller/AdminActivity.java
@@ -2,17 +2,45 @@ package com.example.alex.secondhandcarseller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import java.util.ArrayList;
 
 public class AdminActivity extends AppCompatActivity {
+
+
+    private ArrayList<String> DealerType = new ArrayList<>();
+    private ArrayList<String> DealerNo = new ArrayList<>();
+    private RecyclerView recycleViewAllDealer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+        recycleViewAllDealer=(RecyclerView) findViewById(R.id.recycleViewAllDealer);
+
+        DealerType.add("Pending Dealers");
+        DealerNo.add("32");
+        DealerType.add("Blacklisted Dealers");
+        DealerNo.add("52");
+        DealerType.add("Invalid Dealers");
+        DealerNo.add("21");
+
+        initRecyclerView();
 
         setTitle("Admin");
     }
 
+    private void initRecyclerView() {
+
+        AdminAdapter adapter = new AdminAdapter(DealerType,DealerNo,this);
+        recycleViewAllDealer.setAdapter(adapter);
+        recycleViewAllDealer.setLayoutManager(new LinearLayoutManager(this));
+
+    }
     @Override
     public void onBackPressed() {
         finish();
---
+++ app/src/main/java/com/example/alex/secondhandcarseller/AdminAdapter.java
@@ -0,0 +1,83 @@
+package com.example.alex.secondhandcarseller;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Alex on 11/11/2018.
+ */
+
+public class AdminAdapter  extends RecyclerView.Adapter<AdminAdapter.ViewHolder> {
+    private static final String TAG = "AdminAdapter";
+
+    private ArrayList<String> DealerType = new ArrayList<>();
+    private ArrayList<String> DealerNo = new ArrayList<>();
+    private Context mContext;
+
+    public AdminAdapter(ArrayList<String> dealerType, ArrayList<String> dealerNo, Context mContext) {
+        DealerType = dealerType;
+        DealerNo = dealerNo;
+        this.mContext = mContext;
+    }
+
+
+    @NonNull
+    @Override
+    public AdminAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_dealer, parent, false);
+        AdminAdapter.ViewHolder holder =new AdminAdapter.ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdminAdapter.ViewHolder holder, final int position) {
+        Log.d(TAG, "onBindViewHolder: called.");
+
+
+        holder.tvNumberDealer.setText(DealerNo.get(position));
+        holder.tvDealerTitle.setText(DealerType.get(position));
+
+        holder.Dealer_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(DealerNo.get(position).equals(0)){
+
+                }else if(DealerNo.get(position).equals(1)){
+
+                }
+                else {
+
+                }
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return DealerType.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvDealerTitle, tvNumberDealer;
+        ConstraintLayout Dealer_layout;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            tvNumberDealer = (TextView) itemView.findViewById(R.id.tvNumberDealer);
+            tvDealerTitle = (TextView) itemView.findViewById(R.id.tvDealerTitle);
+            Dealer_layout = itemView.findViewById(R.id.Dealer_layout);
+        }
+    }
+}
